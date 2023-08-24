53e85fbab3d65494a82cd43b8618eb7f5ff75f4e
tais defante <taisadefante@hotmail.com>
2023-08-23 22:37:12 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function cadastrar() {
+    const nome = document.getElementById('nome').value
+    const tel = document.getElementById('telefone').value
+    const email = document.getElementById('email').value
+    const end = document.getElementById('endereco').value
+    
+    alert(`Nome: ${nome}\nTelefone: ${tel}\nEmail: ${email}\nEndereço: ${end}`)
+
+}
